061e9c108274e6e7788584020dc6a54f5f2ca89c
elvis gabriel <egmartinez@ids.com.mx>
2017-06-13 13:44:33 -0500

    ok2

--- vida/js/app.js
+++ vida/js/app.js
@@ -5,13 +5,26 @@ $(document).ready(function(){
 		yearRange: "1940:2004",
 		dateFormat: 'd-m-yy',
 		inline: true,
-		onSelect: function() { 
+		onSelect: function() {
 			var date = $(".selector").val();
 			splitDate = date.split("-")
-			day = splitDate[0];
 			$('#day').val(splitDate[0])
 			$('#moth').val(splitDate[1])
 			$('#year').val(splitDate[2])
+		} 
+    });	
+    $('.selector2').datepicker({
+		changeMonth: true,
+		changeYear: true, 	
+		yearRange: "1940:2004",
+		dateFormat: 'd-m-yy',
+		inline: true,
+		onSelect: function() {
+			var date2 = $(".selector2").val();
+			splitDate2 = date2.split("-")
+			$('#day2').val(splitDate2[0])
+			$('#moth2').val(splitDate2[1])
+			$('#year2').val(splitDate2[2])
 		} 
     });	
 })
